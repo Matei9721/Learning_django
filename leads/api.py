bc8203fa8455d65ebc2e100c5744fcff2ffc1a98
Matei Penca <matei.penca1@gmail.com>
2020-11-20 15:11:18 +0100

    population the DB in api.py

--- leads/api.py
+++ leads/api.py
@@ -1,9 +1,9 @@
 from rest_framework.filters import OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 
-from leads.models import Lead, Movie, Countries
+from leads.models import Lead, Movie, Countries, Actors
 from rest_framework import viewsets, permissions
-from .serializers import LeadSerializer, MovieSerializer
+from .serializers import LeadSerializer, MovieSerializer, CountriesSerializer
 
 import json
 
@@ -31,10 +31,29 @@ class MovieViewSet(viewsets.ModelViewSet):
     serializer_class = MovieSerializer
 
 
+class CountriesViewSet(viewsets.ModelViewSet):
+    queryset = Countries.objects.all()
+    serializer_class = CountriesSerializer
+
+
 json_data = open('leads/fixtures/movie.json')
 data1 = json.load(json_data)  # deserialises it
 data2 = json.dumps(data1)  # json formatted string
 
 json_data.close()
+
+#################### Populate the database
+for film in data1:
+    Movie.objects.update_or_create(title=film['title'], rating=film['rating'], year=film['year'],
+                                   user_rating=film['users_rating'],
+                                   votes=film['votes'],
+                                   metascore=film['metascore'], img_url=film['img_url'],
+                                   description=film['description'],
+                                   runtime=film['runtime'], imdb_url=film['imdb_url'], tagline=film['tagline'])
+    mov = Movie.objects.create(title='Test din json')
+    for nume_tara in film['countries']:
+        mov.countries.create(country_name=nume_tara)
+    for nume_actor in film['actors']:
+        mov.actors.create(actor_name=nume_actor)
 
 
--- leads/serializers.py
+++ leads/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from leads.models import Lead, Movie
+from leads.models import Lead, Movie, Countries, Actors
 
 
 class LeadSerializer(serializers.ModelSerializer):
@@ -8,7 +8,25 @@ class LeadSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class CountriesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Countries
+        fields = ['country_name']
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actors
+        fields = ['actor_name']
+
+
 class MovieSerializer(serializers.ModelSerializer):
+    countries = CountriesSerializer(read_only=True, many=True)
+    actors = ActorSerializer(read_only=True, many=True)
+
     class Meta:
         model = Movie
         fields = '__all__'
+
+
+
